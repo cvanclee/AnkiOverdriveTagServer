8bb033c743831b6eac3305ebcc2a97f05bfe9867
Christian Van Cleef <cvanclee@oswego.edu>
2018-09-13 15:23:33 -0400

    Server logging and vehicle connect attempt

--- src/cvc/capstone/MainClass.java
+++ src/cvc/capstone/MainClass.java
@@ -6,8 +6,8 @@ import de.adesso.anki.AnkiConnector;
 
 public class MainClass {
 
-	public static final int PORT = 5000;
-	public static final String NODE_JS_SERVER_IP = "localhost";
+	public static final int NODE_JS_SERVER_PORT = 5000;
+	public static final String NODE_JS_SERVER_NAME = "localhost";
 
 	public static void main(String[] args) {
 		AnkiConnector anki = null;
@@ -15,12 +15,13 @@ public class MainClass {
 
 		System.out.println("Launching connectors...");
 		try {
-			anki = new AnkiConnector(NODE_JS_SERVER_IP, PORT);
+			anki = new AnkiConnector(NODE_JS_SERVER_NAME, NODE_JS_SERVER_PORT);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Unable to create AnkiConnector. Exiting");
 			return;
 		}
+		
 		System.out.println("Looking for cars...");
 		try {
 			vehicles = anki.findVehicles();
@@ -32,6 +33,7 @@ public class MainClass {
 		System.out.println("Found " + vehicles.size() + " cars:");
 		for (Vehicle v : vehicles) {
 			System.out.println(v.getAdvertisement().getModelId());
+			v.connect();
 		}
 	}
 }
